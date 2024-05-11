f6f3e3fa086f5091e26473bd298a9089aee531ae
Opurbo Kumar <poprap333@gmail.com>
2024-05-11 22:40:38 +0600

    Basic dataframe operation

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from ucimlrepo import fetch_ucirepo
+import pandas as pd
 
-# fetch dataset
-iris = fetch_ucirepo(id=53)
+data = pd.read_csv('D:\\iris.data', names=["sepal length", "sepal width", "petal length", "petal width", "class"])
 
-# data (as pandas dataframes)
-X = iris.data.features
-y = iris.data.targets
+#prints first 5 rows of the dataset.
+print(data.head())
 
-# metadata
-print(iris.metadata)
 
-# variable informatio
-print(iris.variables)
+#prints a specific row from the dataset
+print(data.iloc[45])
+
+#prints a specific range from the dataset
+print(data.iloc[90:98])
+
+#prints a specific column of a specific row
+print(data.at[69, "petal width"])
+
+#printing all the row's petal width
+for i in range(145):
+    print(data.at[i, "petal width"])
+
+#printing all the row's class
+for i in range(145):
+    print(data.at[i, "class"])
+
+#adding a new row to the dataset
+data.loc[len(data)] = [4.5,8,9,4,"iris_seo"]
+
+
+#printing new dataset's tail
+print(data.tail())
+
